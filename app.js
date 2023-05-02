d5b3780792a5daa2ef048b8b4c7b4e98a3b4be7e
Franciele-Matos <francielematos22@outlook.com>
2023-05-01 21:54:01 -0300

    adicionando console log

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("A aplicação está se conectando ao banco de dados")
+// mais um console log de teste;
+console.log("Commit teste")
+console.log("Enviando para o GitHub")
